7ae3b355d99ee2be6b1d7f5ae85409ad04006135
unn3ceserry <staak118@gmail.com>
2025-12-10 11:41:50 +0200

    feat: shader

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -3,6 +3,7 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 import { configApp } from "../config/conig-app";
 import StoreProvider from "../store/StoreProvider";
+import { ShaderDarkVeil } from "@/shared/ui";
 
 const fontRoboto = Roboto({
   variable: "--font-roboto",
@@ -25,7 +26,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${fontRoboto.className} antialiased`}>
-        <StoreProvider>{children}</StoreProvider>
+        <StoreProvider>
+          <div className="w-full h-screen fixed">
+            <ShaderDarkVeil speed={2} hueShift={245} noiseIntensity={0.1} scanlineFrequency={3.8} scanlineIntensity={1} warpAmount={1} />
+          </div>
+          {children}
+        </StoreProvider>
       </body>
     </html>
   );
