6da3984779d0b543daee54216a827cff714e79e3
unn3ceserry <staak118@gmail.com>
2025-12-11 13:56:35 +0200

    feat: proxy routes

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,5 +1,26 @@
 import { routing } from "@/shared";
 import createMiddleware from "next-intl/middleware";
+import { NextRequest, NextResponse } from "next/server";
+
+export function proxy(request: NextRequest) {
+  const isAuth = request.cookies.get("session");
+  const pathname = request.nextUrl.pathname;
+
+  const locale = pathname.split("/")[1];
+  const path = pathname.replace(`/${locale}`, "") || "/";
+
+  const publicRoutes = ["/", "/auth", "/register"];
+  const privateRoutes = ["/c"];
+
+  if (publicRoutes.includes(path) && isAuth) {
+    return NextResponse.redirect(new URL(`/${locale}/c`, request.url));
+  }
+
+  if (privateRoutes.includes(path) && !isAuth) {
+    return NextResponse.redirect(new URL(`/${locale}`, request.url));
+  }
+  return NextResponse.next();
+}
 
 export default createMiddleware(routing);
 
