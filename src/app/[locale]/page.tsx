3a49e2e16dc047d0644a9da181f8cb72008772cb
unn3ceserry <staak118@gmail.com>
2025-12-11 13:44:10 +0200

    fix: trubles with title template page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,14 +1,22 @@
-import type { Metadata } from 'next'
-import { getTranslations } from 'next-intl/server';
-
-export async function generateMetadata({ params }: { params: { locale: string } }): Promise<Metadata> {
-  const { locale } = await params;
-  const t = await  getTranslations({locale})
-  return {
-    title: t('home.title'),
-  };
-}
+import { GlassReactangle } from "@/shared";
+import type { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
 
 export default function Page() {
-  return <div></div>
+  return (
+    <div className="flex items-center justify-center h-full w-full">
+      {/* <GlassReactangle
+        displacementScale={70}
+        blurAmount={0.01}
+        cornerRadius={110}
+        className="ring ring-accent/15"
+      >
+        <input
+          type="text"
+          className="p-3 text-text/50 text-[.8rem]"
+          placeholder="Enther your phone"
+        ></input>
+      </GlassReactangle> */}
+    </div>
+  );
 }
