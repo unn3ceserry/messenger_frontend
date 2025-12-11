b389b17a3344c240f3f1680447aad7de7446f709
unn3ceserry <staak118@gmail.com>
2025-12-11 16:59:36 +0200

    feat: zustand & user complete data store

---
+++ src/app/index.ts
@@ -0,0 +1,14 @@
+export {
+  type UserCompleteDataStore,
+  getUserData,
+  setUserCompleteData,
+  useUserCompleteDataStore,
+  type AppDispatch,
+  type AppStore,
+  type RootState,
+  makeStore,
+  useAppDispatch,
+  useAppSelector,
+  useAppStore,
+} from "./store";
+export { configApp } from "./config";
