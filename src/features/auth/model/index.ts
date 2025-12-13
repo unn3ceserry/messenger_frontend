23bb44b85ceca4b5b8513f81faf56e4a7ec91436
unn3ceserry <staak118@gmail.com>
2025-12-13 13:31:08 +0200

    feat: auth schemas & rhf & zod

--- src/features/auth/model/index.ts
+++ src/features/auth/model/index.ts
@@ -1,2 +1,3 @@
 export type {SignInType, SignUpType} from './types/AuthTypes'
 export {handleAuthUser} from './handlers/handleAuthUser'
+export {schemaSignIn, schemaSignUp} from './types/authSchemas'
