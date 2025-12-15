cd2bf483b3b2b22e8bcd6b08161085114acaff66
unn3ceserry <staak118@gmail.com>
2025-12-15 11:58:28 +0200

    remove: compelte page & fix auth schema

--- src/widgets/ui/auth/register/RegisterTitle.tsx
+++ src/widgets/ui/auth/register/RegisterTitle.tsx
@@ -3,11 +3,13 @@
 import { useTranslations } from "next-intl";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
-import {RegisterFormNumber} from "@/features";
+import { useEffect, useState } from "react";
+import { RegisterFormNumber } from "@/features";
+import CompleteProfile from "../../complete/CompleteProfile";
 
 const RegisterTitle = () => {
   const t = useTranslations();
+  const [isVisible, setIsVisible] = useState<boolean>(false);
 
   useEffect(() => {
     AOS.init({
@@ -17,20 +19,25 @@ const RegisterTitle = () => {
   }, []);
 
   return (
-    <div
-      data-aos="zoom-out"
-      className="flex flex-col items-start justify-center gap-5 w-full max-w-100"
-    >
-      <div className="flex flex-col items-start justify-center gap-1">
-        <h1 className="text-2xl text-white">{t("register.title")}</h1>
-        <p className="text-white text-[.8rem]">{t("register.desc")}</p>
-      </div>
+    <>
+      {isVisible ? (
+        <CompleteProfile />
+      ) : (
+        <div
+          data-aos="zoom-out"
+          className="flex flex-col items-start justify-center gap-5 w-full max-w-100"
+        >
+          <div className="flex flex-col items-start justify-center gap-1">
+            <h1 className="text-2xl text-white">{t("register.title")}</h1>
+            <p className="text-white text-[.8rem]">{t("register.desc")}</p>
+          </div>
 
-      {/* form auth */}
-      <RegisterFormNumber/>
-    </div>
+          {/* form auth */}
+          <RegisterFormNumber setIsVisible={setIsVisible} />
+        </div>
+      )}
+    </>
   );
 };
-
 
 export default RegisterTitle;
