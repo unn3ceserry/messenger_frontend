a5a19ee5a0429e60064eb36c475dd9d12e35c9c3
unn3ceserry <staak118@gmail.com>
2026-01-04 12:44:17 +0200

    feat: search input

---
+++ src/shared/ui/input/SearchInput.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { FC, InputHTMLAttributes, ReactNode } from "react";
+
+interface IDefaultInput extends InputHTMLAttributes<HTMLInputElement> {
+  icon?: ReactNode;
+  classNameDiv?: string;
+}
+const SearchInput: FC<IDefaultInput> = ({
+  className,
+  classNameDiv,
+  icon,
+  ...props
+}) => {
+  return (
+    <div
+      className={`${
+        classNameDiv ? classNameDiv : ""
+      } flex items-center justify-center gap-3 p-3 bg-white/5 backdrop-blur-xl rounded-full ring-2 ring-white/7 focus-within:ring-accent duration-500 w-full text-white`}
+    >
+      {icon}
+      <input
+        {...props}
+        className={`${
+          className ? className : ""
+        } text-white outline-none flex-1 placeholder:text-white/50 w-full`}
+      />
+    </div>
+  );
+};
+
+export default SearchInput;
