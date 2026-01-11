daa02df63e45443922f776596d4864957e2fde67
unn3ceserry <staak118@gmail.com>
2026-01-11 09:41:17 +0200

    feat: Checkbox Default

---
+++ src/shared/ui/checkbox/CheckboxDefault.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { createRipple } from "@/shared/lib";
+import { Check } from "lucide-react";
+import { FC } from "react";
+
+interface ICheckboxDefault {
+  isActive: boolean;
+  onClick: () => void;
+  content: string;
+}
+
+const CheckboxDefault: FC<ICheckboxDefault> = ({
+  content,
+  isActive,
+  onClick,
+}) => {
+  return (
+    <div
+      onClick={(e) => {
+        onClick(), createRipple(e);
+      }}
+      className="relative overflow-hidden hover:bg-white/5 rounded-2xl px-5 py-3 flex items-center justify-start gap-5 cursor-pointer w-full"
+    >
+      {/* checkbox */}
+      <div
+        className={`w-5 h-5 rounded-md flex items-center justify-center ${!isActive ? 'border-2 border-white/40' : ''}`}
+      >
+        {isActive && <div className="w-5 h-5 bg-accent rounded-md flex items-center justify-center"><Check size={17} /></div>}
+      </div>
+      <p>{content}</p>
+    </div>
+  );
+};
+
+export default CheckboxDefault;
