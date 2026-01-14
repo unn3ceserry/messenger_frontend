27314ae4a59c5a32c4fa6ecc9360eb0dde076660
unn3ceserry <staak118@gmail.com>
2026-01-14 16:24:03 +0200

    feat: bio

--- src/entities/user/ui/UserProfile.tsx
+++ src/entities/user/ui/UserProfile.tsx
@@ -4,7 +4,7 @@ import { userApi } from "@/entities";
 import { motion } from "framer-motion";
 import { useTranslations } from "use-intl";
 import { RenderAvatarElement } from "@/shared";
-import {UserSettingsShortInfo} from "@/widgets";
+import { UserSettingsShortInfo } from "@/widgets";
 import UserProfileHeader from "./elements/userprofile/UserProfileHeader";
 import UserProfileLastAvatars from "./elements/userprofile/UserProfileLastAvatars";
 
@@ -28,8 +28,7 @@ const UserProfile = () => {
         className="w-full flex flex-col items-center justify-start"
       >
         <UserProfileHeader />
-        <div className="w-full flex flex-col items-center justify-center p-2 gap-3">
-
+        <div className="w-full flex flex-col items-start justify-center p-2 gap-3">
           {/* avatar */}
           <div className="flex flex-col items-center w-full gap-3 cursor-pointer p-3">
             <RenderAvatarElement
@@ -53,6 +52,13 @@ const UserProfile = () => {
             username={data.username}
             email={data.email}
           />
+
+          <hr className="w-full border-3 border-black/15" />
+
+          <div className="flex flex-col items-start justify-center text-start px-2 gap-1">
+            <h2 className="text-white/50 text-[1.1rem]">{t("profile.bio")}:</h2>
+            <p className="text-[.9rem]">{data.bio ? data.bio : t("profile.noBioYet")}</p>
+          </div>
 
           <hr className="w-full border-3 border-black/15" />
 
