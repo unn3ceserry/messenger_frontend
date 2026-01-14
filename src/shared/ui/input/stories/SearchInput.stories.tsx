c32ecd4f18e46723d21ae2afd34acdf479f98cc7
unn3ceserry <staak118@gmail.com>
2026-01-14 12:45:51 +0200

    change: size icons in search input stories

--- src/shared/ui/input/stories/SearchInput.stories.tsx
+++ src/shared/ui/input/stories/SearchInput.stories.tsx
@@ -44,8 +44,8 @@ export const EmailInput: Story = {
     icon: (
       <svg
         xmlns="www.w3.org"
-        width="24"
-        height="24"
+        width="22"
+        height="22"
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
@@ -80,8 +80,8 @@ export const PasswordInput: Story = {
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
-        width="24"
-        height="24"
+        width="22"
+        height="22"
         fill="currentColor"
       >
         <path d="M12 17a2 2 0 0 0 2-2v-2h-4v2a2 2 0 0 0 2 2zm6-9h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10z" />
@@ -107,8 +107,8 @@ export const UsernameInput: Story = {
   args: {
     icon: (
       <svg
-        width="24"
-        height="24"
+        width="22"
+        height="22"
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
