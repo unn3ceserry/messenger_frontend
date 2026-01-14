54a5a794682dba23fde541c258f744312dfe25a3
unn3ceserry <staak118@gmail.com>
2026-01-14 12:56:41 +0200

    feat: checkbox default stories

---
+++ src/shared/ui/checkbox/stories/CheckboxDefault.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from "@storybook/nextjs-vite";
+
+import CheckboxDefault from "../CheckboxDefault";
+import { fn } from "storybook/test";
+import { useState } from "react";
+
+const meta = {
+  title: "UIComponents/CheckboxDefault",
+  component: CheckboxDefault,
+  parameters: {
+    layout: "centered",
+    docs: {
+      codePanel: true,
+    },
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+  args: { onClick: fn() },
+} satisfies Meta<typeof CheckboxDefault>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+
+export const Checkbox: Story = {
+  args: {
+    content: "Test checkbox circle",
+    isActive: true,
+  },
+  render: (args) => {
+    const [active, setActive] = useState<boolean>(true);
+
+    return (
+      <CheckboxDefault
+        {...args}
+        isActive={active}
+        onClick={() => setActive((prev) => !prev)}
+      />
+    );
+  },
+};
