eff22f4196078bc4bc5b32299cb37e3e98ff400f
unn3ceserry <staak118@gmail.com>
2026-01-27 17:34:43 +0200

    feat: bg pattern

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,6 +8,7 @@ import { notFound } from "next/navigation";
 import { getTranslations, setRequestLocale } from "next-intl/server";
 import { ToastContainer } from "react-toastify";
 import ThemeProvider from "../providers/ThemeProvider";
+import bg from "@/app/assets/imgs/pattern.svg";
 
 const fontRoboto = Roboto({
   variable: "--font-roboto",
@@ -51,6 +52,18 @@ export default async function RootLayout({
         <StoreProvider>
           <ThemeProvider>
             <NextIntlClientProvider locale={locale}>
+              <div
+                className="absolute inset-0 w-full h-full bg-linear-to-b from-accent/70 to-accent/20"
+                style={{
+                  WebkitMaskImage: `url(${bg.src})`,
+                  WebkitMaskRepeat: "repeat",
+                  WebkitMaskSize: "350px",
+                  maskImage: `url(${bg.src})`,
+                  maskRepeat: "repeat",
+                  maskSize: "350px",
+                }}
+              ></div>
+
               {children}
               <ToastContainer position="bottom-right" />
             </NextIntlClientProvider>
