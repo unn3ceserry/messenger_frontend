977404a97038e005ca5d64a054347a4085558944
unn3ceserry <staak118@gmail.com>
2026-01-28 12:18:37 +0200

    refactor: refactor chatui

--- src/widgets/ui/chat/ChatUI.tsx
+++ src/widgets/ui/chat/ChatUI.tsx
@@ -12,6 +12,7 @@ import { useAppDispatch, useAppSelector } from "@/app";
 import { useResizingSlice, setWidth, handleMouseMove } from "@/features";
 import { motion } from "framer-motion";
 import ChatUICompoonent from "./ChatUICompoonent";
+import ChatUIUserProfileComponent from "./ChatUIUserProfileComponent";
 
 const MIN_WIDTH = 300;
 const MAX_WIDTH = 680;
@@ -65,33 +66,7 @@ const ChatUI = () => {
           className="w-0.5 bg-line-color self-stretch cursor-e-resize"
         ></div>
       </div>
-      <AnimatePresence mode="wait">
-        {!!isOpenOtherOsersProfile.component && (
-          <motion.div
-            initial={{ x: "100%" }}
-            animate={{ x: "0%" }}
-            exit={{ x: "100%" }}
-            transition={{ duration: 0.25 }}
-            layout
-            className="fixed right-0 top-0 z-12313 h-screen max-w-100 w-full bg-chatui-bg overflow-y-auto text-default-text-color scrollbar-thin otherprofile:border-l border-line-color shadow-[0_0px_30px_-8px_rgba(0,0,0,0.8)]"
-          >
-            {(() => {
-              switch (isOpenOtherOsersProfile.component) {
-                case "userProfile":
-                  return (
-                    <OtherUsersProfile
-                      username={'durov'}
-                    />
-                  );
-                case "editContact":
-                  return (
-                    <EditContact username={'durov'} />
-                  );
-              }
-            })()}
-          </motion.div>
-        )}
-      </AnimatePresence>
+      <ChatUIUserProfileComponent/>
     </div>
   );
 };
