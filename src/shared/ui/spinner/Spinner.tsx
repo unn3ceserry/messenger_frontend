266c9cdce027b6e21b4cea26f53947133fb4ecff
unn3ceserry <staak118@gmail.com>
2026-01-28 13:43:48 +0200

    feat: Spinner animation

--- src/shared/ui/spinner/Spinner.tsx
+++ src/shared/ui/spinner/Spinner.tsx
@@ -1,10 +1,20 @@
 "use client";
 
+import { motion, AnimatePresence } from "framer-motion";
+
 const Spinner = () => {
   return (
-    <div className="flex items-center justify-center mx-auto">
-      <div className="w-10 h-10 border-4 border-t-accent border-icons-color/30 rounded-full animate-spin"></div>
-    </div>
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{duration: 0.5}}
+        className="flex items-center justify-center mx-auto"
+      >
+        <div className="w-10 h-10 border-4 border-t-accent border-icons-color/30 rounded-full animate-spin"></div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
