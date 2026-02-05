21299d1d2cb742b0dfd7ba13a9df2a1a221fc4a7
unn3ceserry <staak118@gmail.com>
2026-02-05 20:05:10 +0200

    feat: anim on user search and chats sidebar

--- src/entities/chats/ui/LeftSideBar/LeftSideBar.tsx
+++ src/entities/chats/ui/LeftSideBar/LeftSideBar.tsx
@@ -1,17 +1,11 @@
 "use client";
 
 import { useAppSelector } from "@/app";
-import {
-  ActionsPopup,
-  chatsApi,
-  LeftSideBarSearch,
-  UserType,
-} from "@/entities";
+import { ActionsPopup, LeftSideBarSearch, UserType } from "@/entities";
 import { SearchUsers, selectOpenComponent } from "@/entities/user";
 import { AnimatePresence } from "framer-motion";
 import { Dispatch, FC, SetStateAction, useState } from "react";
-import ChatItem from "../ChatItem/ChatItem";
-import { Spinner } from "@/shared";
+import ChatsSideBar from "../Chats/SideBar/ChatsSideBar";
 
 interface ILeftSideBar {
   isOpen: boolean;
@@ -23,10 +17,6 @@ const LeftSideBar: FC<ILeftSideBar> = ({ isOpen, setIsOpen, data }) => {
   const [searchText, setSearchText] = useState<string>("");
   const isOpenSearchUsers = useAppSelector(selectOpenComponent);
 
-  const { data: dataDms, isLoading } = chatsApi.useGetMyDmsQuery();
-
-  if (isLoading || !dataDms) return <Spinner />;
-
   return (
     <>
       <div className="flex flex-col items-center justify-start gap-1 h-screen text-white w-full overflow-y-auto scrollbar-thin">
@@ -36,32 +26,12 @@ const LeftSideBar: FC<ILeftSideBar> = ({ isOpen, setIsOpen, data }) => {
           setSearchText={setSearchText}
           isOpenSearchUsers={isOpenSearchUsers}
         />
-        {isOpenSearchUsers && searchText && (
+        {isOpenSearchUsers && searchText ? (
           <SearchUsers searchText={searchText} setSearchText={setSearchText} />
+        ) : (
+          <ChatsSideBar userId={data.id} />
         )}
-        <div className="flex flex-col w-full p-2">
-          {dataDms.map((el) => {
-            const user = el.members?.filter((el) => el.userId !== data.id)[0]
-              .user;
-            return (
-              <ChatItem
-                firstName={user?.firstName}
-                lastName={user?.lastName}
-                hasAvatar={!!user?.avatars.length}
-                avatar={
-                  user?.avatars ? user.avatars[user.avatars.length - 1] : ""
-                }
-                size={50}
-                key={el.id}
-                message={
-                  el.messages ? el.messages[el.messages.length - 1].text : ""
-                }
-              />
-            );
-          })}
-        </div>
       </div>
-
       {/* actions modal */}
       <AnimatePresence>
         {isOpen && (
