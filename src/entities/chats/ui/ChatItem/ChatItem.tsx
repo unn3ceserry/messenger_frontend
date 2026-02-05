8e433101403f4c38872800cae61f9d39d3504280
unn3ceserry <staak118@gmail.com>
2026-02-05 19:08:53 +0200

    feat: getMyDms and ChatItem component

---
+++ src/entities/chats/ui/ChatItem/ChatItem.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { createRipple, RenderAvatarElement } from "@/shared";
+import { useTranslations } from "next-intl";
+import { FC } from "react";
+
+interface Props {
+  hasAvatar: boolean;
+  avatar?: string;
+  size: number;
+  message: string;
+  firstName?: string;
+  lastName?: string;
+}
+
+const ChatItem: FC<Props> = ({ hasAvatar, size, avatar, message, firstName, lastName }) => {
+
+  const t = useTranslations();
+
+  return (
+    <div
+      onClick={(e) => createRipple(e)}
+      className="flex w-full items-start gap-3 cursor-pointer relative hover:bg-checkbox-hover rounded-2xl p-2.5 overflow-hidden"
+    >
+      <RenderAvatarElement hasAvatar={hasAvatar} size={size} avatar={avatar} />
+      <div className="flex flex-col items-start justify-center">
+        <h2>{firstName} {lastName}</h2>
+        <p className="line-clamp-1 text-icons-color text-[0.85rem]">
+          {message ? message : t('searchUsers.noResult')}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ChatItem;
--- src/entities/chats/ui/LeftSideBar/LeftSideBar.tsx
+++ src/entities/chats/ui/LeftSideBar/LeftSideBar.tsx
@@ -1,10 +1,17 @@
 "use client";
 
 import { useAppSelector } from "@/app";
-import { ActionsPopup, LeftSideBarSearch, UserType } from "@/entities";
+import {
+  ActionsPopup,
+  chatsApi,
+  LeftSideBarSearch,
+  UserType,
+} from "@/entities";
 import { SearchUsers, selectOpenComponent } from "@/entities/user";
 import { AnimatePresence } from "framer-motion";
 import { Dispatch, FC, SetStateAction, useState } from "react";
+import ChatItem from "../ChatItem/ChatItem";
+import { Spinner } from "@/shared";
 
 interface ILeftSideBar {
   isOpen: boolean;
@@ -16,9 +23,13 @@ const LeftSideBar: FC<ILeftSideBar> = ({ isOpen, setIsOpen, data }) => {
   const [searchText, setSearchText] = useState<string>("");
   const isOpenSearchUsers = useAppSelector(selectOpenComponent);
 
+  const { data: dataDms, isLoading } = chatsApi.useGetMyDmsQuery();
+
+  if (isLoading || !dataDms) return <Spinner />;
+
   return (
     <>
-      <div className="flex flex-col items-center justify-start gap-1 h-screen text-white w-full">
+      <div className="flex flex-col items-center justify-start gap-1 h-screen text-white w-full overflow-y-auto scrollbar-thin">
         <LeftSideBarSearch
           setIsOpen={setIsOpen}
           searchText={searchText}
@@ -28,6 +39,27 @@ const LeftSideBar: FC<ILeftSideBar> = ({ isOpen, setIsOpen, data }) => {
         {isOpenSearchUsers && searchText && (
           <SearchUsers searchText={searchText} setSearchText={setSearchText} />
         )}
+        <div className="flex flex-col w-full p-2">
+          {dataDms.map((el) => {
+            const user = el.members?.filter((el) => el.userId !== data.id)[0]
+              .user;
+            return (
+              <ChatItem
+                firstName={user?.firstName}
+                lastName={user?.lastName}
+                hasAvatar={!!user?.avatars.length}
+                avatar={
+                  user?.avatars ? user.avatars[user.avatars.length - 1] : ""
+                }
+                size={50}
+                key={el.id}
+                message={
+                  el.messages ? el.messages[el.messages.length - 1].text : ""
+                }
+              />
+            );
+          })}
+        </div>
       </div>
 
       {/* actions modal */}
