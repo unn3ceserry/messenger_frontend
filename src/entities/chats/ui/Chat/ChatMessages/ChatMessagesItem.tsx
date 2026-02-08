f2cf7d52bd5a620276502f305319b3406233f05b
unn3ceserry <staak118@gmail.com>
2026-02-08 14:10:42 +0200

    feat: when press enter will send message

--- src/entities/chats/ui/Chat/ChatMessages/ChatMessagesItem.tsx
+++ src/entities/chats/ui/Chat/ChatMessages/ChatMessagesItem.tsx
@@ -11,7 +11,7 @@ interface Props {
 const ChatMessagesItem: FC<Props> = ({ isMy, message, createdAt }) => {
   return (
     <div className={`flex text-white w-full ${isMy ? 'justify-end' : 'justify-start'}`}>
-      <div className={`p-2.5 rounded-t-2xl 
+      <div className={`p-2 py-1.5 rounded-t-2xl 
         ${isMy ? 'rounded-bl-2xl bg-accent' : 'rounded-br-2xl bg-chatui-bg'}
         w-max max-w-[70%] break-all
       `}>
