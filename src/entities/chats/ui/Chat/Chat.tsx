ca99d6769bda9a97fe8793d2386ecfe325c86bb2
unn3ceserry <staak118@gmail.com>
2026-02-09 22:28:52 +0200

    feat: get user data in chat

--- src/entities/chats/ui/Chat/Chat.tsx
+++ src/entities/chats/ui/Chat/Chat.tsx
@@ -2,29 +2,28 @@
 
 import { useAppSelector } from "@/app";
 import { getCurrentChat } from "@/entities/chats/model";
-import { setOpenComponentOtherUsersProfile } from "@/entities/user";
+import { setOpenComponentOtherUsersProfile, userApi } from "@/entities/user";
 import { RenderAvatarElement, Spinner } from "@/shared";
 import { useTranslations } from "next-intl";
 import { FC } from "react";
 import { useDispatch } from "react-redux";
 import ChatInput from "./ChatInput/ChatInput";
 import ChatMessages from "./ChatMessages/ChatMessages";
-import { chatsApi } from "../../api";
 
 interface Props {
   userId: string;
 }
 
 const Chat: FC<Props> = ({ userId }) => {
-  const currentChat = useAppSelector(getCurrentChat);
-  
-  if (!currentChat) return <Spinner />;
-
-  const user = currentChat.members?.find((m) => m.userId !== userId)?.user;
-  const avatar = user?.avatars[user.avatars.length - 1];
-
   const t = useTranslations();
   const dispatch = useDispatch();
+  const currentChat = useAppSelector(getCurrentChat);
+
+  const user = currentChat?.members?.find((m) => m.userId !== userId)?.user;
+  const {data, isLoading} = userApi.useGetUserDataQuery({id: user?.id ?? ''})
+  const avatar = data?.avatars ? data?.avatars[data?.avatars.length - 1] : '';
+
+  if(!currentChat || isLoading) return <Spinner/>
 
   return (
     <div className="flex flex-col items-center justify-between h-screen w-full text-default-text-color gap-5">
@@ -33,20 +32,20 @@ const Chat: FC<Props> = ({ userId }) => {
           dispatch(
             setOpenComponentOtherUsersProfile({
               openComponent: "userProfile",
-              username: user?.username ?? "",
+              username: data?.username ?? "",
             }),
           )
         }
         className="flex w-full items-center justify-start bg-chatui-bg p-2 px-5 gap-3 cursor-pointer"
       >
         <RenderAvatarElement
-          hasAvatar={!!user?.avatars.length}
+          hasAvatar={!!data?.avatars?.length}
           size={40}
           avatar={avatar}
         />
         <div className="flex flex-col items-start justify-center w-full">
           <h2 className="">
-            {user?.firstName} {user?.lastName}
+            {data?.firstName} {data?.lastName}
           </h2>
           <p className="text-icons-color text-[.85rem]">
             {t("settings.online")}
@@ -54,11 +53,11 @@ const Chat: FC<Props> = ({ userId }) => {
         </div>
       </div>
 
-      <ChatMessages userId={user?.id ?? ''} />
+      <ChatMessages userId={data?.id ?? ''} />
 
       {/* input */}
       <div className="flex w-full items-center justify-center px-5 max-w-175">
-        <ChatInput userId={user?.id ?? ''} />
+        <ChatInput userId={data?.id ?? ''} />
       </div>
     </div>
   );
