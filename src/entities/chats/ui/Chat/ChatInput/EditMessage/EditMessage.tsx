f8654c1c780c205850f2478694be958d4937a432
unn3ceserry <staak118@gmail.com>
2026-02-18 15:50:30 +0200

    fix: long string renderer

--- src/entities/chats/ui/Chat/ChatInput/EditMessage/EditMessage.tsx
+++ src/entities/chats/ui/Chat/ChatInput/EditMessage/EditMessage.tsx
@@ -10,20 +10,22 @@ import { Pencil, X } from "lucide-react";
 import { useTranslations } from "next-intl";
 
 const EditMessage = () => {
-  const t = useTranslations()
+  const t = useTranslations();
   const disptach = useAppDispatch();
   const editingMessage = useAppSelector(getEditingMessage);
 
-  if(!editingMessage) return <Spinner/>
+  if (!editingMessage) return <Spinner />;
   return (
     <div className="flex w-full p-1 items-center justify-between gap-3">
       <Pencil size={21} className="text-blue-400" />
-      <div className="flex flex-col items-start justify-center w-full bg-blue-400/10 rounded-sm p-0.5 px-2 border-l-[3px] border-l-blue-400">
-        <p className="text-[.85rem] text-blue-400">{t('chat.editing')}</p>
-        <p className="text-[.75rem]">
+      <div className="flex flex-col items-start justify-center w-full min-w-0 bg-blue-400/10 rounded-sm p-0.5 px-2 border-l-[3px] border-l-blue-400">
+        <p className="text-[.85rem] text-blue-400">{t("chat.editing")}</p>
+
+        <p className="text-[.75rem] truncate break-all max-w-full">
           {editingMessage.text}
         </p>
       </div>
+
       <X
         onClick={() => disptach(removeEditingMessage())}
         size={24}
