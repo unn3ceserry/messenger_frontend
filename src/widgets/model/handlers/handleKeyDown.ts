ffd63319594308ecbe44284da1c5c5934a04fd95
unn3ceserry <staak118@gmail.com>
2026-02-18 19:00:17 +0200

    feat: close settings

--- src/widgets/model/handlers/handleKeyDown.ts
+++ src/widgets/model/handlers/handleKeyDown.ts
@@ -1,14 +1,22 @@
 import { makeStore } from "@/app";
-import { closeCurrentChat, removeEditingMessage } from "@/entities";
+import {
+  closeCurrentChat,
+  openComponent,
+  removeEditingMessage,
+} from "@/entities";
 
 export const handleKeyDown = (e: KeyboardEvent) => {
   const isEditingMessage = makeStore.getState().chats.editMessage;
   const currentChat = makeStore.getState().chats.currentChat;
-
+  const currentOpenComponent =
+    makeStore.getState().userUiOpenComponent.openComponent;
+  console.log(currentOpenComponent);
   switch (e.key) {
     case "Escape":
       e.preventDefault();
-      if (!!isEditingMessage) {
+      if (!!currentOpenComponent) {
+        makeStore.dispatch(openComponent(null));
+      } else if (!!isEditingMessage) {
         makeStore.dispatch(removeEditingMessage());
       } else if (!!currentChat) {
         makeStore.dispatch(closeCurrentChat());
