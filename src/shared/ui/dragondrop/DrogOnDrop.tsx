8efda8093589276e6fc2af03f46a1e56ebdefe4d
unn3ceserry <staak118@gmail.com>
2026-03-12 15:29:29 +0200

    feat: onDrop handler

--- src/shared/ui/dragondrop/DrogOnDrop.tsx
+++ src/shared/ui/dragondrop/DrogOnDrop.tsx
@@ -2,12 +2,16 @@
 
 import { File } from "lucide-react";
 import { useTranslations } from "next-intl";
-import { DragEvent, useState } from "react";
-import {motion} from 'framer-motion'
+import { Dispatch, DragEvent, FC, SetStateAction, useState } from "react";
+import { motion } from "framer-motion";
 
-const DrogOnDrop = () => {
+interface Props {
+  setIsDrag: Dispatch<SetStateAction<boolean>>;
+  setFiles: Dispatch<SetStateAction<Array<File>>>;
+}
 
-  const t = useTranslations()
+const DrogOnDrop: FC<Props> = ({ setFiles, setIsDrag }) => {
+  const t = useTranslations();
   const [isHover, setIsHover] = useState<boolean>(false);
 
   const handleOnDragOver = (e: DragEvent<HTMLDivElement>) => {
@@ -22,14 +26,24 @@ const DrogOnDrop = () => {
     }
   };
 
+  const handleOnDrop = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDrag(false);
+    Array.from(e.dataTransfer.files).map((file) => {
+      setFiles((prev) => [...prev, file]);
+    });
+    // console.log(files);
+  };
+
   return (
     <motion.div
-      initial={{opacity: 0}}
-      exit={{opacity: 0}}
-      animate={{opacity: 1}}
-      transition={{duration: .2}}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.2 }}
       onDragOver={(e) => handleOnDragOver(e)}
       onDragLeave={(e) => handleOnDragLeave(e)}
+      onDrop={(e) => handleOnDrop(e)}
       className="absolute inset-5 max-w-200 bg-chatui-bg rounded-2xl flex items-center justify-center flex-col mx-auto p-5"
     >
       <div
