b1fbec2d24fd7f605f465bd60928cbc367bec26c
unn3ceserry <staak118@gmail.com>
2026-03-13 22:45:11 +0200

    Update index.ts

--- src/entities/chats/model/index.ts
+++ src/entities/chats/model/index.ts
@@ -1,4 +1,4 @@
-export type { Chat, ChatMember, Message } from "./types/chatsTypes";
+export type { Chat, ChatMember, Message, Attachment } from "./types/chatsTypes";
 export { useChatSocket } from "./hooks/useChatSocket";
 export { useMessageSocket } from "./hooks/useMessageSocket";
 export {
