2fda9cb8380989e26ef308270c75db8cfff58261
unn3ceserry <staak118@gmail.com>
2026-04-06 15:22:35 +0300

    fix: node version & path storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,16 @@
-import type { StorybookConfig } from '@storybook/nextjs-vite';
+import type { StorybookConfig } from "@storybook/nextjs-vite";
+import path from "path";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
     "@storybook/addon-a11y",
     "@storybook/addon-docs",
-    "@storybook/addon-onboarding"
+    "@storybook/addon-onboarding",
   ],
-  "framework": "@storybook/nextjs-vite",
-  "staticDirs": [
-    "..\\public"
-  ]
+  framework: "@storybook/nextjs-vite",
+  staticDirs: [path.join(__dirname, "..", "public")],
 };
 export default config;
